f1dec4ab62ac5d41a86fe7f9152663f0c9c51285
Samantha Cabrera <samantha.n.cabrera@gmail.com>
2024-02-21 15:25:44 -0700

    complete

--- index.js
+++ index.js
@@ -11,15 +11,17 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-
 const titleCased = () => tutorials.map(function (str) {
+  // splits string into individual words
   const words = str.split(' ');
-  
+
   const word = words.map(function (word) {
+    // checks if first letter needs to be capitalized or not
     if (word.charAt(0) === word.charAt(0).toLowerCase()) {
       word = word.charAt(0).toUpperCase() + word.slice(1);
     }
     return (word);
   })
+  // combines separate words into one string
   return (word.toString().concat().replaceAll(',', ' '));
 })
